7557ac82ca2dd81928aa14c113129a40d2b14ef5
victor barelli <vbarelli1@gmail.com>
2025-11-27 10:45:28 -0300

    feat: PipeLine Auto sem vpc

--- ec2.tf
+++ ec2.tf
@@ -31,6 +31,7 @@ resource "aws_vpc_security_group_ingress_rule" "allow_ssh" {
   to_port           = 22
 }
 
+
 resource "aws_vpc_security_group_ingress_rule" "allow_http" {
   security_group_id = aws_security_group.website_sg.id
   cidr_ipv4         = "0.0.0.0/0"
